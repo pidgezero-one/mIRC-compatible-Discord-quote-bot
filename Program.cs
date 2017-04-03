679360321a6024506fe573d2bb6ee02a10137d66
pidgezero_one <s.kischak@gmail.com>
2017-04-02 23:03:09 -0400

    initial commit
    Signed-off-by: pidgezero_one <s.kischak@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using System.IO;
+
+namespace Pidgler
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().Start();
+
+
+        public void Start()
+        {
+            var bot = new Discord.DiscordClient();
+            
+            bot.MessageReceived += async (s, e) =>
+            {
+                var _server = e.Server;
+                var _channel = e.Channel;
+                var _user = e.User;
+                char[] charSeparator = new char[] { ' ' };
+                string[] ex = e.Message.RawText.Trim().Split(charSeparator); 
+                if (e.Message.RawText.StartsWith("!addquote"))
+                {
+                    var UserPerms = _user.Roles;
+					//This restricts quote-adding to only people with a certain role.
+					//Replace YOUR_ROLE_ID with the role ID of whatever you want it restricted to. This is a number with no quote marks.
+					//If you don't want it restricted, change the following like to say true instead of false, and then remove the "foreach" statement.
+                    Boolean hasPerm = false;
+                    foreach (Role role in UserPerms)
+                    {
+                        if (role.Id == YOUR_ROLE_ID)
+                        {
+                            hasPerm = true;
+                        }
+                    }
+                    if (hasPerm)
+                    {
+                        if (ex.Length > 1)
+                        {
+                            String[] arr = ex.Skip(1).ToArray();
+                            List<string> lines = new List<string>();
+                            lines.Add(String.Join(" ", arr).Trim());
+                            File.AppendAllLines(@"path\to\your\quotes\textfile.txt", lines);
+                            await e.Channel.SendMessage("Quote added.");
+                            Console.WriteLine("Quote added: " + String.Join(" ", arr).Trim());
+                        }
+                        else
+                        {
+                            await e.Channel.SendMessage("Don't add a blank quote you dingus");
+                        }
+                    }
+                    else
+                    {
+                        await e.Channel.SendMessage("This function is restricted to users with special roles");
+                    }
+                }
+                else if (e.Message.RawText.StartsWith("!quote"))
+                {
+                    string[] lines = System.IO.File.ReadAllLines(@"path\to\your\quotes\textfile.txt");
+                    var max = lines.Length;
+
+                    if (lines.Length > 0)
+                    {
+
+                        if (ex.Length > 1)
+                        {
+                            String[] arr = ex.Skip(1).ToArray();
+                            var searchTerm = String.Join(" ", arr).Trim();
+                            List<String> returnedResults = new List<String>();
+
+                            for (int i = 0; i < max; i++)
+                            {
+                                if (lines[i].ToLower().Contains(searchTerm.ToLower()))
+                                {
+                                    returnedResults.Add(lines[i]);
+                                }
+                            }
+                            if (returnedResults.Count > 0)
+                            {
+                                int index = new Random().Next(0, returnedResults.Count);
+                                await e.Channel.SendMessage("Quote: " + returnedResults[index]);
+                            }
+                            else
+                            {
+                                await e.Channel.SendMessage("No quotes found matching " + searchTerm);
+                            }
+                        }
+                        else
+                        {
+                            int index = new Random().Next(0, max);
+                            string line = lines[index];
+                            await e.Channel.SendMessage("Quote: " + line);
+                        }
+                    }
+                    else
+                    {
+                        await e.Channel.SendMessage("Quotes DB is empty.");
+                    }
+                }
+            };
+
+            bot.ExecuteAndWait(async () =>
+            {
+                await bot.Connect("YOUR_BOT_TOKEN", TokenType.Bot);
+
+            });
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # mIRC-compatible-Discord-quote-bot
 A basic random quote database bot for Discord servers that accesses text file quote DBs usually operated by mIRC quote bots
+
+This quotes bot stores quotes in a text file. The reason to do this instead of using a better service like SQLite is because this bot is built to work with mIRC-based bots such as [my channel command twitch bot](https://github.com/pidgezero-one/SMRPG-Contest-Bot), which generally use text files for quotes.
+
+#How to Use
+
+Step 1: Go to https://discordapp.com/developers/applications/me and create a new application. Then choose the option to register a bot user. You will be given a bot token. In the file "Program.cs", replace "YOUR_BOT_TOKEN" with your bot token (surrounded by quote marks).
+
+Step 2: If you want to restrict the ability to add quotes to only users with a certain role in your server, you must replace YOUR_ROLE_ID with the number that is the role ID you want. (If you don't know this number, go into your User Settings on Discord, go into Appearance, and enable Developer Mode. Then in your server, mention the role you want to apply it to, and right click your message to copy the role ID to your clipboard.) If you don't want to restrict this function, follow the instructions in program.cs to remove this requirement.
+
+Step 3: Find the txt file where your mIRC bot stores quotes and replace every instance of @"path\to\your\quotes\textfile.txt" with the full system directory to the quotes file. This will usually be in the directory where your mIRC.exe is located.
+
+Step 4: Build or run the project in Microsoft Visual Studio 2017.
+
+Then people with the right permissions can use **!addquote *text to add*** to save quotes, and others can use **!quote** to get a random quote or **!quote *text to search*** to get a random quote matching a search phrase.
